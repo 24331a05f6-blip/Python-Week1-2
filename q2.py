96ef716d27679306f309d54c968ed9e9bbbbbfea
Chinni Krishna <24331a05f6@mvgrce.edu.in>
2025-12-14 21:56:18 +0530

    Add files via upload

---
+++ q2.py
@@ -0,0 +1,10 @@
+#24331A05F6
+# python program to perform the following expressions using operator precedence
+a=5+3*2
+b=2*3**2
+c=2**3**2
+d=(2**3)**2
+print("Value of a:",a)
+print("Value of b:",b)
+print("Value of c:",c)
+print("Value of d:",d)
